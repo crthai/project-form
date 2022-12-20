3ce7b9cd54eac447b16bfec6c96023f5b4aa97c7
Gab0 Gomes <gabriel.gomes@clickbus.com>
2022-12-20 14:39:10 -0300

    feat: review of 20-12-2022

--- script.js
+++ script.js
@@ -1,69 +1,109 @@
-var a_parent = document.querySelectorAll(".a_parent");
-const form = document.getElementById("form");
-const name = document.getElementById("name");
-const email = document.getElementById("email");
+const $form = document.getElementById("form");
 
-a_parent.forEach(function (a_parent_item) {
-  a_parent_item.addEventListener("click", function () {
-    a_parent.forEach(function (a_parent_item) {
-      a_parent_item.classList.remove("active");
+const setupEvents = () => {
+  setupNavLinkEvents();
+  setupNavMenuEvents();
+};
+
+const setupNavLinkEvents = () => {
+  const $navLinks = document.querySelectorAll(".a_parent");
+  $navLinks.forEach(function ($navLink) {
+    $navLink.addEventListener("mouseenter", function () {
+      $navLinks.forEach(($internalNavLink) =>
+        $internalNavLink.classList.remove("active")
+      );
+      $navLink.classList.add("active");
     });
-    a_parent_item.classList.add("active");
   });
-});
+};
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkInputs();
+const setupNavMenuEvents = () => {
+  const $navMenus = document.querySelectorAll(".menu");
+  $navMenus.forEach(function ($navMenu) {
+    $navMenu.addEventListener("mouseleave", function () {
+      const $navLink = $navMenu.parentElement.querySelector(".a_parent");
+      $navLink.classList.remove("active");
+    });
+  });
+};
+
+$form.addEventListener("submit", (e) => {
+  try {
+    e.preventDefault();
+    checkInputs()
+      .then(() => {
+        alert(
+          "O usuário " +
+            name.value +
+            "foi registrado com o e-mail " +
+            email.value
+        );
+      })
+      .catch(() => {
+        console.log("Oops...");
+      });
+  } catch (e) {
+    console.log("Oops 2...");
+  }
 });
 
 function checkInputName() {
   //get the values from the inputs and remove whitespace
-  const nameValue = name.value.trim();
-  const checkInput = new Promise((resolve) => {
-    if (nameValue === "") {
-      setErrorFor(name, "Name cannot be blank.");
-    } else {
-      setSuccessFor(name);
+  return new Promise((resolve, reject) => {
+    const $name = document.getElementById("name");
+    const nameValue = $name.value.trim();
+    if (!nameValue) {
+      return setErrorFor($name, "Name cannot be blank.").then(() => {
+        reject(false);
+      });
     }
+
+    return setSuccessFor($name).then(() => {
+      resolve(true);
+    });
   });
 }
+
 function checkInputEmail() {
   //get the values from the inputs and remove whitespace
-  const emailValue = email.value.trim();
-  const checkInput = new Promise((resolve) => {
-    if (emailValue === "") {
-      setErrorFor(email, "Email cannot be blank.");
-    } else {
-      setSuccessFor(email);
+  return new Promise((resolve, reject) => {
+    const $email = document.getElementById("email");
+    const emailValue = $email.value.trim();
+    if (!emailValue) {
+      return setErrorFor($email, "Email cannot be blank.").then(() => {
+        reject(false);
+      });
     }
+    return setSuccessFor($email).then(() => {
+      resolve(true);
+    });
   });
 }
 
-async function checkInputs() {
-  try {
-    const checkFirstInput = await checkInputName();
-    const checkSecondaryInput = await checkInputEmail();
-    alert(
-      "O usuário " + name.value + "foi registrado com o e-mail " + email.value
-    );
-  } catch {
-    console.log("Ops...");
-  }
+function checkInputs() {
+  return Promise.all([checkInputName(), checkInputEmail()]);
 }
 
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-
-  small.innerText = message;
-
-  formControl.className = "form-control error";
+function setErrorFor($input, message) {
+  return new Promise((resolve) => {
+    const $formControl = $input.closest('.form-control');
+    const $small = $formControl.querySelector("small");
+    $small.innerText = message;
+    $formControl.className = "form-control error";
+    return resolve(true);
+  });
 }
 
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-
-  formControl.className = "form-control success";
+function setSuccessFor($input) {
+  return new Promise((resolve) => {
+    const $formControl = $input.closest('.form-control');
+    $formControl.className = "form-control success";
+    setTimeout(() => {
+      resolve(true);
+    }, 100);
+  });
 }
+
+window.addEventListener("load", (event) => {
+  setupEvents();
+});
