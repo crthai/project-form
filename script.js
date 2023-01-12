3824d1d8427195b78bdf663c20cf3ff4114f577a
Gab0 Gomes <gabriel.gomes@clickbus.com>
2023-01-12 14:12:17 -0300

    feat: improvements on form validation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const setupEvents = () => {
   setupNavLinkEvents();
   setupNavMenuEvents();
@@ -28,96 +27,24 @@ const setupNavMenuEvents = () => {
 };
 
 const showSucessAlert = ($form) => {
-  setTimeout(function() {   
+  setTimeout(function () {
     const name = $form.querySelector('input[name="name"]').value;
     const email = $form.querySelector('input[name="email"]').value;
-    alert(`O usuário ${name} foi registrado com o e-mail ${email}`); }, 
-  1);
-}
+    alert(`O usuário ${name} foi registrado com o e-mail ${email}`);
+  }, 1);
+};
 
 const setupSubmitEvent = () => {
   const $form = document.getElementById("form");
   if ($form) {
     $form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const validForm = validateForm($form);
-        if(validForm){
-          showSucessAlert($form);
-        }
+      e.preventDefault();
+      const validForm = validator.validateForm($form);
+      if (validForm) {
+        showSucessAlert($form);
+      }
     });
   }
-};
-
-function setErrorFor($input, message) {
-    const $formControl = $input.closest(".form-control");
-    const $messageError = $formControl.querySelector(".message-error");
-    $messageError.textContent = message;
-    $formControl.className = "form-control error";
-}
-
-function setSuccessFor($input) {
-    const $formControl = $input.closest(".form-control");
-    $formControl.className = "form-control success";
-}
-
-const fieldValidations = {
-  email: ["required", "validEmail"],
-  name: ["required"]
-};
-
-const validateByType = {
-  required: (value) => {
-    return value;
-  },
-  validEmail: (value) => {
-    var regex = /\S+@\S+\.\S+/;
-    return regex.test(value);
-  }
-}
-
-const getErrorMessageByValidationType = {
-  required: (fieldName) => `${fieldName} cannot be blank.`,
-  validEmail: () => `Invalid email format`
-};
-
-const buildErrorMessage = (fieldErrors, inputName) => {
-  const firstValidationFailed = fieldErrors[0];
-  const errorMessage = getErrorMessageByValidationType[firstValidationFailed](inputName)
-  return errorMessage;
-}
-
-const checkFieldValidations = (inputName, inputValue) => {
-  const validations = fieldValidations[inputName];
-  let fieldErrors = []; 
-  validations.forEach((validation) => {
-    if(!validateByType[validation](inputValue)){
-      fieldErrors.push(validation);
-    }
-  })
-  return fieldErrors;
-}
-
-const validateForm = ($form) => {
-  const $inputs = $form.querySelectorAll("input");
-  let formIsValid = true;
-
-  $inputs.forEach(($input) => {
-    const inputName = $input.name;
-    const inputValue = $input.value;
-    const fieldErrors = checkFieldValidations(inputName, inputValue);
-
-    const fieldHasErrors = fieldErrors.length > 0;
-    if (fieldHasErrors){
-      formIsValid = false;
-      const errorMessage = buildErrorMessage(fieldErrors, inputName);
-      setErrorFor($input, errorMessage);
-      return;
-    }
-   
-    setSuccessFor($input);
-  });
-
-  return formIsValid;
 };
 
 window.addEventListener("load", (event) => {
