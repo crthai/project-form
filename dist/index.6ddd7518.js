b7664cad8c96fd96f206a31c959045f5aabd804d
Taissa <taissacrfreire@gmail.com>
2023-01-17 10:05:02 -0300

    .

---
+++ dist/index.6ddd7518.js
@@ -0,0 +1,65 @@
+const validator = {
+    setErrorFor: ($input, message)=>{
+        const $formControl = $input.closest(".form-control");
+        const $messageError = $formControl.querySelector(".message-error");
+        $messageError.textContent = message;
+        $formControl.className = "form-control error";
+    },
+    setSuccessFor: ($input)=>{
+        const $formControl = $input.closest(".form-control");
+        $formControl.className = "form-control success";
+    },
+    validatorByType: {
+        required: {
+            validate: ($input)=>{
+                return $input.value.length > 0;
+            },
+            message: ($input)=>`${$input.name} cannot be blank.`
+        },
+        name: {
+            validate: ($input)=>{
+                return $input.value.length > 0;
+            },
+            message: ($input)=>`${$input.name} cannot be blank.`
+        },
+        email: {
+            validate: ($input)=>{
+                var regex = /\S+@\S+\.\S+/;
+                return regex.test($input.value);
+            },
+            message: ()=>`Invalid email format`
+        }
+    },
+    validateForm: ($form)=>{
+        const $inputs = $form.querySelectorAll("input");
+        return validator.validateInputs($inputs);
+    },
+    validateInputs: ($inputs)=>{
+        let areAllInputsValid = true;
+        $inputs.forEach(($input)=>{
+            const isValidInput = validator.validateEachStep($input);
+            areAllInputsValid &= isValidInput;
+        });
+        return areAllInputsValid;
+    },
+    validateEachStep: ($input)=>{
+        const VT_KEY = "vtValidations";
+        let allStepsAreValid = true;
+        const hasStepsToValidate = $input.dataset[VT_KEY];
+        if (hasStepsToValidate) {
+            const steps = $input.dataset[VT_KEY].split("|");
+            steps.forEach((type)=>{
+                try {
+                    allStepsAreValid &= validator.validatorByType[type].validate($input);
+                    if (!allStepsAreValid) validator.setErrorFor($input, validator.validatorByType[type].message($input));
+                } catch (error) {
+                    throw new Error("Ops, aconteceu um erro na valida\xe7\xe3o do campo", error);
+                }
+            });
+        }
+        if (allStepsAreValid) validator.setSuccessFor($input);
+        return allStepsAreValid;
+    }
+};
+
+//# sourceMappingURL=index.6ddd7518.js.map
---
+++ dist/index.72be8890.js
@@ -0,0 +1,53 @@
+const navMenu = document.getElementById("nav-menu"), toggleMenu = document.getElementById("toggle-menu"), closeMenu = document.getElementById("close-menu");
+toggleMenu.addEventListener("click", ()=>{
+    navMenu.classList.toggle("show");
+});
+closeMenu.addEventListener("click", ()=>{
+    navMenu.classList.remove("show");
+});
+const setupEvents = ()=>{
+    setupNavLinkEvents();
+    setupNavMenuEvents();
+    setupSubmitEvent();
+};
+function toggleMobileMenu(menu) {
+    menu.classList.toggle("open");
+}
+const setupNavLinkEvents = ()=>{
+    const $navLinks = document.querySelectorAll(".nav-link");
+    $navLinks.forEach(function($navLink) {
+        $navLink.addEventListener("mouseenter", function() {
+            $navLinks.forEach(($internalNavLink)=>$internalNavLink.classList.remove("active"));
+            $navLink.classList.add("active");
+        });
+    });
+};
+const setupNavMenuEvents = ()=>{
+    const $navMenus = document.querySelectorAll(".nav-menu");
+    $navMenus.forEach(function($navMenu) {
+        $navMenu.addEventListener("mouseleave", function() {
+            const $navLink = $navMenu.parentElement.querySelector(".nav-link");
+            $navLink.classList.remove("active");
+        });
+    });
+};
+const showSucessAlert = ($form)=>{
+    setTimeout(()=>{
+        const name = $form.querySelector('input[name="name"]').value;
+        const email = $form.querySelector('input[name="email"]').value;
+        alert(`O usuário ${name} foi registrado com o e-mail ${email}`);
+    }, 1);
+};
+const setupSubmitEvent = ()=>{
+    const $form = document.getElementById("form");
+    if ($form) $form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        const validForm = validator.validateForm($form);
+        if (validForm) showSucessAlert($form);
+    });
+};
+window.addEventListener("load", (event)=>{
+    setupEvents();
+});
+
+//# sourceMappingURL=index.72be8890.js.map
