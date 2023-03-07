cd958d748b7d4113dd5b7299ed31113110f2d4a9
Gab0 Gomes <gabriel.gomes@clickbus.com>
2023-03-07 14:31:01 -0300

    review do dia 07-03

--- cypress/e2e/form.spec.cy.js
+++ cypress/e2e/form.spec.cy.js
@@ -1,16 +1,20 @@
+/* eslint-disable no-undef */
 describe('template spec', () => {
-  it('should pass', () => {
+  it('should register user if all data is correct', () => {
     cy.visit('http://localhost:1234/');
     cy.get('#name').type('Fulana da Silva');
     cy.get('#email').type('fulaninha@gmail.com');
     cy.get('#button-submit').click();
-  })
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('O usuário Fulana da Silva foi registrado com o e-mail fulaninha@gmail.com');
+    });
+  });
 
-  it('should fail and show a error message', () => {
+  it('should fail and show a error message when data is invalid', () => {
     cy.visit('http://localhost:1234/');
     cy.get('#email').type('fulaninha@teste.com');
     cy.get('#button-submit').click();
 
-    cy.get('.message-error').should("contain.text", "name cannot be blank.")
-  })
-})
+    cy.get('.message-error').should('contain.text', 'name cannot be blank.');
+  });
+});
---
+++ src/js/validator.test.js
@@ -0,0 +1,64 @@
+import { validator } from './validator';
+
+describe('InputValidation', () => {
+  it('should validate the name correctly', () => {
+    const inputName = {
+      Fulana: {
+        value: 'Fulana',
+        name: 'FulanaField',
+      },
+      Cicrana: {
+        value: '',
+        name: 'CicranaField',
+      },
+    };
+    const isNameValid = validator.validatorByType.name.validate(inputName.Fulana);
+    expect(isNameValid).toEqual(true);
+
+    const isCicranaNameValid = validator.validatorByType.name.validate(inputName.Cicrana);
+    expect(isCicranaNameValid).toEqual(false);
+    expect(validator.validatorByType.name.message(inputName.Cicrana)).toEqual('CicranaField cannot be blank.');
+  });
+
+  it('should validate the email correctly', () => {
+    const inputEmail = [
+      {
+        value: 'fulana@teste.com.br',
+        name: 'FulanaFieldEmail',
+        expected: true,
+      },
+      {
+        value: 'cicranateste',
+        name: 'CicranaFieldEmail',
+        expected: false,
+      },
+      {
+        value: 'fulaninhagmail.com',
+        name: 'emailWithoutAt',
+        expected: false,
+      },
+      {
+        value: 'fulaninha@gmailcom',
+        name: 'emailWithoutDot',
+        expected: false,
+      },
+      {
+        value: '.fulaninhagmail@com',
+        name: 'emailWithoutFormat',
+        expected: false,
+      },
+    ];
+    expect.assertions(inputEmail.reduce(
+      (acc, testCase) => (testCase.expected ? acc + 1 : acc + 2),
+      0,
+    ));
+
+    inputEmail.forEach((testCase) => {
+      const isEmailValid = validator.validatorByType.email.validate(testCase);
+      expect(isEmailValid).toEqual(testCase.expected);
+      if (!isEmailValid) {
+        expect(validator.validatorByType.email.message(testCase)).toEqual('Invalid email format');
+      }
+    });
+  });
+});
